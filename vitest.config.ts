a798d6c5a10d930b4f9ecefdceb20fc00f4da140
sadoffToxir <toxa.saidoff@gmail.com>
2023-03-04 14:48:01 +0100

    vitest basic configuration

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+
+export default defineConfig({
+  plugins: [ vue(), vuetify() ],
+  test:{
+    globals:true, 
+    environment: 'happy-dom', 
+  },
+  resolve: {
+    alias:{
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './'),
+    }
+  }
+})
