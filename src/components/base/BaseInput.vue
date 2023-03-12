868eaddf8714ddbffa403ffa57dfd01b824d8ba8
sadoffToxir <toxa.saidoff@gmail.com>
2023-03-12 14:46:08 +0100

    default settings

--- src/components/base/BaseInput.vue
+++ src/components/base/BaseInput.vue
@@ -1,12 +1,10 @@
 <template>
   <v-textarea 
-    :id="id"
     :model-value="modelValue"
     :auto-grow="noResize ? undefined : autoGrow"
     :clearable="clearable"
     :density="density"
     clear-icon="mdi-close-circle"
-    :direction="direction"
     :disabled="disabled"
     :error="error"
     :error-messages="errorMessages"
@@ -14,13 +12,11 @@
     :hint="hint"
     :label="label"
     :loading="loading"
-    :max-errors="maxErrors"
     :no-resize="true"
     :persistent-hint="persistentHint"
     :placeholder="placeholder"
     :append-icon="appendIcon"
     :append-inner-icon="appendInnerIcon"
-    :readonly="readonly"
     :rows="rows"
     @update:model-value="handleChange"
   />
@@ -28,29 +24,41 @@
 
 <script setup lang="ts">
 withDefaults(defineProps<{
-  modelValue: string
-  autoGrow: boolean
-  clearable: boolean
-  density: 'default' | 'comfortable' | 'compact'
-  direction: 'horizontal' | 'vertical'
-  disabled: boolean
-  error: boolean
-  errorMessages: string | string[]
-  hideDetails: 	boolean | 'auto'
-  hint: string
-  id: string
-  label: string
-  loading: boolean
-  maxErrors: string | number
-  name: string
-  noResize: boolean
-  persistentHint: boolean
-  placeholder: string
-  appendIcon: string 
-  appendInnerIcon: string 
-  readonly: boolean
-  rows: string | number
+  modelValue?: string
+  autoGrow?: boolean
+  clearable?: boolean
+  density?: 'default' | 'comfortable' | 'compact'
+  disabled?: boolean
+  error?: boolean
+  errorMessages?: string | string[]
+  hideDetails?: 	boolean | 'auto'
+  hint?: string
+  label?: string
+  loading?: boolean
+  name?: string
+  noResize?: boolean
+  persistentHint?: boolean
+  placeholder?: string
+  appendIcon?: string 
+  appendInnerIcon?: string 
+  rows?: string | number
 }>(), { 
+  modelValue: '',
+  clearable: false,
+  density: 'default',
+  disabled: false,
+  error: false,
+  errorMessages: '',
+  hideDetails: 'auto',
+  hint:'',
+  label: '',
+  loading: false,
+  name: '',
+  noResize: false,
+  persistentHint: false,
+  placeholder: '',
+  appendIcon: '',
+  appendInnerIcon: '',
   autoGrow: true,
   rows: 1
 })
