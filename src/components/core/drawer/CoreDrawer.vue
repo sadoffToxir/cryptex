77152c0db0c69cd7c7fce13f2079e467c4a7dede
Tokhir Saidov <toxa.saidoff@gmail.com>
2023-06-18 20:06:54 -0700

    coreDrawer

---
+++ src/components/core/drawer/CoreDrawer.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card>
+    <v-navigation-drawer
+      v-model="drawer"
+      :rail="rail"
+      permanent
+      @click="rail = false"
+    >
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+        title="John Leider"
+        nav
+      >
+        <template #append>
+          <v-btn
+            variant="text"
+            icon="mdi-chevron-left"
+            @click.stop="rail = !rail"
+          />
+        </template>
+      </v-list-item>
+
+      <v-divider />
+
+      <v-list
+        density="compact"
+        nav
+      >
+        <v-list-item
+          prepend-icon="mdi-home-city"
+          title="Home"
+          value="home"
+        />
+        <v-list-item
+          prepend-icon="mdi-account"
+          title="My Account"
+          value="account"
+        />
+        <v-list-item
+          prepend-icon="mdi-account-group-outline"
+          title="Users"
+          value="users"
+        />
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref(true)
+const rail = ref(true)
+</script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,8 +2,13 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css'
+
 
 export const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
   components,
   directives,
 });
