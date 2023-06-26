9f8eabf57df24329bdb3ab82263646792479b698
Tokhir Saidov <toxa.saidoff@gmail.com>
2023-06-26 08:43:23 -0700

    firebase finish

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyA5YcpVIdaojuBYOk-EPxBmt0eA8u9kso0',
-  authDomain: 'cryptex-c453d.firebaseapp.com',
-  projectId: 'cryptex-c453d',
-  storageBucket: 'cryptex-c453d.appspot.com',
-  messagingSenderId: '192996010659',
-  appId: '1:192996010659:web:d08aacec5dcfce2f29ed16',
-  measurementId: 'G-JYMPKWNK5E'
+  apiKey: "AIzaSyBVoPytfLzfeuGlKbzqGdkk-0CuQ9LQzXo",
+  authDomain: "cryptex-64ff7.firebaseapp.com",
+  projectId: "cryptex-64ff7",
+  storageBucket: "cryptex-64ff7.appspot.com",
+  messagingSenderId: "265122656235",
+  appId: "1:265122656235:web:f5c9152b23e44f33fa33ae",
+  measurementId: "G-K3MR27YJYH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
